78d9558553b8225b59860cf88b3d646082d148f5
unknown <amanuit19@gmail.com>
2018-10-28 15:16:56 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { DepartmentComponent } from './department/department.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {RouterModule} from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CseComponent } from './cse/cse.component';
+import { ITComponent } from './it/it.component';
+import { CivilComponent } from './civil/civil.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    DepartmentComponent,
+    AboutComponent,
+    ContactComponent,
+    NavbarComponent,
+    CseComponent,
+    ITComponent,
+    CivilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+{path:'about', component: AboutComponent},
+{path:'contact', component: ContactComponent},
+{path:'department', component: DepartmentComponent},
+{path:'news', component: NewsComponent},
+{path:'cse', component: CseComponent},
+{path:'IT', component: ITComponent},
+{path:'civil', component: CivilComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
